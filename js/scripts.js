63370bcf19f3af133f6cf3d4622ac3cf90d79851
ward chamberlain <wchamberlain89@gmail.com>
2019-01-18 09:29:06 -0800

    it will have back buttons to cycle backwards through questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 $(function(){
 	questionIndex = 1; 
+	
 	$('.btn-info').click(function(){
 		questionIndex++; 
 		$(".current-question").removeClass("current-question");
 		$("#q" + questionIndex).addClass("current-question");
 	});
+
+	$('.btn-default').click(function(){
+		questionIndex--;
+		$(".current-question").removeClass("current-question");
+		$("#q" + questionIndex).addClass("current-question");
+	});
+
 });
